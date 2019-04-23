32aa7dfda8ed76f63c18250e8def7c77b928c75c
franciscor32 <49045887+franciscor32@users.noreply.github.com>
2019-04-23 18:47:05 -0500

    Update matching.py

--- nxGraph_Algorithms/matching.py
+++ nxGraph_Algorithms/matching.py
@@ -1,6 +1,18 @@
+from itertools import combinations 
+from Functions.bool_function import is_independent
+from Functions.global_properties import V,n, E
+from Functions.bool_function import is_matching
+from math import floor 
 import networkx as nx
-from itertools import combinations
-from Functions.bool_functions import *
-from Functions.global_properties import *
 
 
+def matching(G):
+    for k in range(floor(n(G)/2), 1, -1):
+        for M in combinations(E(G),k):
+            if is_matching(G,list(M)) == True:
+                return list(M)
+            
+
+G = nx.erdos_renyi_graph(7,.3)
+nx.draw_networkx(G)
+print(matching(G))
