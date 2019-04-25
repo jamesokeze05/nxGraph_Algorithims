8a2db74df98234d683e0ead2d39d141ba25ab4a8
franciscor32 <49045887+franciscor32@users.noreply.github.com>
2019-04-25 16:47:57 -0500

    Update greedy_coloring.py

--- nxGraph_Algorithms/greedy_coloring.py
+++ nxGraph_Algorithms/greedy_coloring.py
@@ -1,20 +1,18 @@
-from Functions.global_properties import *
-from Functions.local_properties import *
-
+from Functions.global_properties import V
+from Functions.local_properties import neighbors
+import networkx as nx
 
 def greedy_coloring(G):
-    colors = {c: None for v in V(G)}
-    colors ([V(G)][0])=1
+    colors = {v: None for v in V(G)}
+    colors[V(G)[0]] = 1
     for v in V(G):
         if colors[v] == None:
-            N = neighbors(G, v)
+            N = neighbors(G,v)
             bad_colors = [colors[w] for w in N]
             j = 1
-            
             while colors[v] == None:
-               if j not in bad_colors:
-                   
-                   colors[v] = j
-               else:
-                   j += 1
+                if j not in bad_colors:
+                    colors[v] = j
+                else:
+                    j+= 1
     return colors
