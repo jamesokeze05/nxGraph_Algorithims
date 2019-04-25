41b53466a3acd986412f34400ce266ee157f4bce
franciscor32 <49045887+franciscor32@users.noreply.github.com>
2019-04-25 16:59:56 -0500

    Update independence.py

--- nxGraph_Algorithms/independence.py
+++ nxGraph_Algorithms/independence.py
@@ -1,17 +1,18 @@
-import networkx as nx
-from itertools import combinations
-from Functions.bool_functions import is_independent
+from itertools import combinations 
+from Functions.bool_function import is_independent
 from Functions.global_properties import V,n
-
+import networkx as nx
 
 def maximum_independent_set(G):
-    for k in range(n(G),1,-1):
+    for k in range(n(G), 1, -1):
         for S in combinations(V(G), k):
-            if is_independent(G,list(S)) == True:
+            if is_independent(G, list(S)) == True:
                 return list(S)
-         
-            
-            
+
 def independence_number(G):
     return len(maximum_independent_set(G))
+
+D = nx.erdos_renyi_graph(7,.3)
+nx.draw_networkx(D)
+print(independence_number(D))
 
